9ee73790a4f8e3e34b886179543f76e5bec7b871
Ryan Bandler <bandlerr@nextcure.com>
2021-07-27 09:47:07 -0400

    added tests example

---
+++ src/main.py
@@ -0,0 +1,4 @@
+
+def factorial(n: int):
+  return 1 if (n==1 or n==0) else n * factorial(n - 1);
+
---
+++ tests/test.py
@@ -0,0 +1,20 @@
+import unittest
+import os, sys
+sys.path.insert(0, os.path.abspath("."))
+from src import main
+
+class TestFactorial(unittest.TestCase):
+    def test_factorial(self):
+        """
+        Test that it calculate factorials properly
+        """
+        
+        resultA = main.factorial(0)
+        resultB = main.factorial(1)
+        resultC = main.factorial(10)
+        self.assertEqual(resultA, 1)
+        self.assertEqual(resultB, 1)
+        self.assertEqual(resultC, 3628800)
+
+if __name__ == '__main__':
+    unittest.main()
